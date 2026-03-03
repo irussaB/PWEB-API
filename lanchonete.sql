b8a0444539c559a84aae63dadd32b40a97a5b2fa
irussaB <barussidemoraespedrohenrique@gmail.com>
2026-03-03 08:56:45 -0300

    'Aula_4'

--- lanchonete.sql
+++ lanchonete.sql
@@ -1,5 +1,5 @@
-create database lanchonete2;
-use lanchonete2;
+create database lanchonete;
+use lanchonete;
 create table usuarios
     (
         usuid int primary key auto_increment,
@@ -8,3 +8,13 @@ create table usuarios
         ususenha varchar(100),
         usulogado boolean default 0
     );
+
+INSERT INTO usuarios(usunome,usulogin,ususenha)
+VALUE
+('Pedro Henrique Barussi de Moraes','irussaB',MD5(19062008)),
+('João Gabriel Leme Bernardina','JG',MD5(09012009)),
+('João Pedro da Silva Souza','JP',MD5(19102008)),
+('Pedro de Sales Santos','Salisson',MD5(16042008)),
+('Leonardo Camargo Graminha','Grama',MD5(31122007)),
+('Gabriel Pereira Alves','Biel',MD5(32102008))
+;
