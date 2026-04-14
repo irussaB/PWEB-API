84d8f52272cb667715b2a6bc64d510b556891224
irussaB <barussidemoraespedrohenrique@gmail.com>
2026-04-14 08:51:26 -0300

    unificada as API no index.php

--- api/categorias/index.php
+++ api/categorias/index.php
@@ -1,58 +1,49 @@
 <?php
-    require '../app/conexao.php';
+    require '../../app/conexao.php';
+    $pdo = Conexao::conectar();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
     $json = filter_input(INPUT_GET,'json');
     $data = json_decode($json,true);
-    $i = $data['i'];
-    switch ($i) {
-        case 'Create':
-            $pdo = Conexao::conectar();
-            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            $nome = $data['nome'];
+    $op = $data['op'];
+    $nome = $data['nome'];
+    $id = $data['id'];
+    $ativo = $data['ativo'];
+    switch ($op) {
+        case 'i':
             $sql = "insert into categorias(catnome) values(?);";
             $prp = $pdo->prepare($sql);
             $prp->execute([$nome]);
-            Conexao::desconectar();
             break;
-        case 'Read':
-            $pdo = Conexao::conectar();
-            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        case 's':
             $sql = "select catid as id, catnome as nome, catativo as ativo from categorias;";
             $prp = $pdo->prepare($sql);
             $prp->execute();
             $data = $prp->fetchall(PDO::FETCH_ASSOC);
             echo json_encode($data);
-            Conexao::desconectar();
             break;
-        case 'ReadP':
-            $pdo = Conexao::conectar();
-            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        case 'sp':
             $nome = '%'.$data['nome'].'%';
             $sql = "select catid as id, catnome as nome, catativo as ativo from categorias where catnome like ?;";
             $prp = $pdo->prepare($sql);
             $prp->execute([$nome]);
             $data = $prp->fetchall(PDO::FETCH_ASSOC);
             echo json_encode($data);
-            Conexao::desconectar();
             break;
-        case 'Update':
-            $pdo = Conexao::conectar();
-            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            $nome = $data['nome'];
-            $ativo = $data['ativo'];
-            $id = $data['id'];
+        case 'u':
             $sql = "update categorias set catnome = ?, catativo = ? where catid = ? ;";        
             $prp = $pdo->prepare($sql);
             $prp->execute([$nome,$ativo,$id]);
-            Conexao::desconectar();
             break;
-        case 'Delete':
+        case 'd':
             echo "Pendente";
-            break;    
+            break; 
+        default:
+            echo "Parametro Inváilido";
     }
 
+    Conexao::desconectar();
 
-    //{"nome":"valor"}
-    //http://localhost/Projetos_ETEC_PWEB-III_Div1/api/categorias/icategorias.php?json={"nome":"Promoção do dia"}
+// ?json={"op:"sp","id":"0","nome":"P","login":"0","Senha":"0"}
 
 ?>
---
+++ api/usuarios/index.php
@@ -0,0 +1,66 @@
+<?php
+    require '../../app/conexao.php';
+    $pdo = Conexao::conectar();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $json = filter_input(INPUT_GET,'json');
+    $data = json_decode($json,true);
+    $op = $data['op'];
+    $id = $data['id'];
+    $nome = $data['nome'];
+    $login = $data['login'];
+    $senha = $data['senha'];
+    $logado = $data['logado'];
+ 
+    switch ($op) {
+        case 'i':
+            $sql = "insert into usuarios(usunome,usulogin,ususenha) values(?,?,MD5(?));";
+            $prp = $pdo->prepare($sql);
+            $prp->execute([$nome,$login,$senha]);
+            break;
+        case 's':
+            $sql = "select usuid as id, usunome as nome, usulogin as usuario, usulogado as logado from usuarios;";
+            $prp = $pdo->prepare($sql);
+            $prp->execute();
+            $data = $prp->fetchall(PDO::FETCH_ASSOC);
+            echo json_encode($data);
+            break;
+        case 'sp':
+            $nome = '%'.$data['nome'].'%';
+            $sql = "select usuid as id, usunome as nome, usulogin as usuario, usulogado as logado from usuarios where usunome like ?;";
+            $prp = $pdo->prepare($sql);
+            $prp->execute([$nome]);
+            $data = $prp->fetchall(PDO::FETCH_ASSOC);
+            echo json_encode($data);
+            break;
+        case 'u':
+            if(!empty($data['senha'])){
+                $sql = "update usuarios set usunome = ?, usulogin = ?, ususenha = MD5(?), usulogado = ? where usuid = ?;";
+                $prp = $pdo->prepare($sql);
+                $prp->execute([$nome,$login,$senha,$logado,$id]);
+            }
+            else{
+                $sql = "update usuarios set usunome = ?, usulogin = ?, usulogado = ? where usuid = ? ;";        
+                $prp = $pdo->prepare($sql);
+                $prp->execute([$nome,$login,$logado,$id]);
+            }
+            break;
+        case 'd':
+            echo "Pendente";
+            break;    
+        case 'l':
+            $sql = "select usuid as id, usunome as nome, usulogin as usuario, usulogado as logado from usuarios where usulogin = ? and ususenha = MD5(?);";
+            $prp = $pdo->prepare($sql);
+            $prp->execute([$login,$senha]);
+            $data = $prp->fetchall(PDO::FETCH_ASSOC);
+            echo json_encode($data);
+            break; 
+        default:
+            echo "Parametro Inválido";
+        }
+        
+    Conexao::desconectar();
+
+//?json={"op":"sp","id":0,"nome":"P","login":"","senha":"","logado":true}
+
+?>
